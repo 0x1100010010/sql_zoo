98b5df4bdf6030c586321498df201e24830e6036
bashforger <bash.forger@gmail.com>
2020-12-14 03:41:43 +0500

    Finished SQL task

--- zoo.sql
+++ zoo.sql
@@ -89,3 +89,135 @@ SELECT sum(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania
 SELECT continent, COUNT(name) FROM world GROUP BY(continent)
 SELECT continent, COUNT(name) FROM world WHERE population >= 10000000 GROUP BY(continent) 
 SELECT continent FROM world GROUP BY(continent) HAVING SUM(population) >= 100000000
+
+-- https://sqlzoo.net/wiki/The_JOIN_operation
+SELECT matchid, player FROM goal WHERE teamid = 'GER'
+SELECT id,stadium,team1,team2 FROM game WHERE id=1012
+SELECT player,teamid, stadium, mdate FROM game JOIN goal ON (game.id = goal.matchid) WHERE teamid = 'GER'
+SELECT team1, team2, player FROM  game JOIN goal ON (game.id = goal.matchid) WHERE player LIKE 'Mario%'
+SELECT player, teamid, coach, gtime FROM goal JOIN eteam ON (goal.teamid = eteam.id) WHERE gtime<=10
+SELECT mdate, teamname FROM game JOIN eteam ON(game.team1 = eteam.id) WHERE coach = 'Fernando Santos'
+SELECT player FROM game JOIN goal ON(game.id = goal.matchid) WHERE stadium = 'National Stadium, Warsaw'
+SELECT DISTINCT player FROM game JOIN goal ON matchid = id WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid!='GER'
+SELECT teamname,COUNT(teamid) FROM eteam JOIN goal ON id=teamid GROUP BY teamname
+SELECT stadium,COUNT(gtime) FROM goal JOIN game ON id=matchid GROUP BY stadium
+SELECT matchid,mdate,COUNT(teamid) FROM game JOIN goal ON matchid = id WHERE (team1 = 'POL' OR team2 = 'POL') GROUP BY matchid,mdate
+SELECT matchid,mdate,COUNT(teamid) FROM game JOIN goal ON matchid = id WHERE (teamid='GER') GROUP BY matchid,mdate
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+  team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+  FROM game LEFT JOIN goal ON matchid = id 
+GROUP BY mdate,matchid,team1,team2
+
+-- https://sqlzoo.net/wiki/More_JOIN_operations
+SELECT id, title FROM movie WHERE yr=1962
+SELECT yr FROM movie WHERE title='Citizen Kane'
+SELECT id,title, yr FROM movie WHERE title LIKE 'Star Trek%' ORDER BY yr
+SELECT id FROM actor WHERE name= 'Glenn Close'
+SELECT id FROM movie WHERE title='Casablanca'
+SELECT name FROM casting, actor WHERE movieid=(SELECT id FROM movie WHERE title='Casablanca') AND actorid=actor.id
+SELECT name FROM movie, casting, actor WHERE title='Alien' AND movieid=movie.id AND actorid=actor.id
+SELECT title FROM movie, casting, actor WHERE name='Harrison Ford' AND movieid=movie.id AND actorid=actor.id
+SELECT title FROM movie, casting, actor WHERE name='Harrison Ford' AND movieid=movie.id AND actorid=actor.id AND ord<>1
+SELECT title, name FROM movie, casting, actor WHERE yr=1962 AND movieid=movie.id AND actorid=actor.id AND ord=1
+SELECT yr,COUNT(title) FROM movie JOIN casting ON movie.id=movieid JOIN actor ON actorid=actor.id where name='Rock Hudson' GROUP BY yr HAVING COUNT(title) > 2
+SELECT title, name FROM movie, casting, actor
+  WHERE movieid=movie.id AND actorid=actor.id AND ord=1 AND movieid IN
+    (SELECT movieid FROM casting, actor WHERE actorid=actor.id AND name='Julie Andrews')
+
+-- https://sqlzoo.net/wiki/Using_Null
+SELECT name FROM teacher WHERE dept IS NULL
+SELECT teacher.name, dept.name FROM teacher INNER JOIN dept ON (teacher.dept=dept.id)
+SELECT teacher.name, dept.name FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+SELECT teacher.name, dept.name FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
+SELECT name, COALESCE(mobile,'07986 444 2266') FROM teacher
+SELECT teacher.name, COALESCE(dept.name,'None') FROM teacher LEFT JOIN dept ON teacher.dept=dept.id
+SELECT COUNT(teacher.name), COUNT(mobile) FROM teacher
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON teacher.dept=dept.id GROUP BY dept.name
+SELECT name, CASE WHEN dept IN (1,2) THEN 'Sci' ELSE 'Art' END FROM teacher
+SELECT name, CASE WHEN dept IN (1,2)  THEN 'Sci' WHEN dept = 3  THEN 'Art' ELSE 'None' END FROM teacher
+
+-- https://sqlzoo.net/wiki/NSS_Tutorial
+SELECT A_STRONGLY_AGREE FROM nss
+ WHERE question='Q01'
+   AND institution='Edinburgh Napier University'
+   AND subject='(8) Computer Science'
+SELECT institution, subject FROM nss WHERE question='Q15' AND score>=100
+SELECT institution, score FROM nss
+ WHERE question='Q15' AND score<50 AND subject = '(8) Computer Science'
+SELECT subject,SUM(response) FROM nss WHERE question='Q22' AND subject IN ('(8) Computer Science','(H) Creative Arts and Design') GROUP BY subject
+SELECT subject,SUM(response*A_STRONGLY_AGREE/100) FROM nss WHERE question='Q22' AND subject IN ('(8) Computer Science','(H) Creative Arts and Design') GROUP BY subject
+SELECT subject, ROUND(SUM(response*A_STRONGLY_AGREE)/SUM(response),0) FROM nss
+ WHERE question='Q22' AND subject IN ('(8) Computer Science','(H) Creative Arts and Design') GROUP BY subject
+SELECT institution, ROUND(SUM(response*score)/SUM(response),0) score FROM nss
+ WHERE question='Q22' AND (institution LIKE '%Manchester%') GROUP BY institution
+SELECT institution, SUM(sample), SUM(CASE WHEN subject LIKE '(8)%' THEN sample END) comp FROM nss WHERE question='Q01' AND (institution LIKE '%Manchester%') GROUP BY institution
+
+-- https://sqlzoo.net/wiki/Window_functions
+SELECT lastName, party, votes FROM ge WHERE constituency = 'S14000024' AND yr = 2017 ORDER BY votes DESC
+SELECT party, votes, RANK() OVER (ORDER BY votes DESC) as posn FROM ge WHERE constituency = 'S14000024' AND yr = 2017 ORDER BY party
+SELECT yr,party, votes, RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn FROM ge WHERE constituency = 'S14000021' ORDER BY party,yr
+SELECT constituency,party, votes,
+  RANK() OVER (PARTITION BY constituency ORDER BY votes DESC)
+    AS posn FROM ge WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+   AND yr  = 2017 ORDER BY posn,constituency
+SELECT constituency, party
+  FROM ( SELECT constituency,party,
+      RANK() OVER (PARTITION BY constituency ORDER BY votes DESC)
+        AS posn FROM ge WHERE constituency BETWEEN 'S14000021' AND 'S14000026' AND yr  = 2017
+   ) AS ed WHERE posn = 1
+SELECT party,COUNT(1) FROM ( SELECT constituency,party,
+      RANK() OVER (PARTITION BY constituency ORDER BY votes DESC)
+        AS posn FROM ge WHERE constituency LIKE 'S%' AND yr  = 2017
+   ) AS ed WHERE posn = 1 GROUP BY party
+
+-- https://sqlzoo.net/wiki/Self_join
+SELECT COUNT(*) FROM stops
+SELECT id FROM stops WHERE name='Craiglockhart'
+SELECT id, name FROM stops, route WHERE id=stop AND company='LRT' AND num='4'
+SELECT company, num, COUNT(*) FROM route WHERE stop=149 OR stop=53 GROUP BY company, num HAVING COUNT(*)=2
+SELECT a.company, a.num, a.stop, b.stop FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num) WHERE a.stop = 53 AND b.stop=149
+SELECT a.company, a.num, stopa.name, stopb.name FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num) JOIN stops stopa ON (a.stop=stopa.id) JOIN stops stopb ON (b.stop=stopb.id)
+    WHERE stopa.name='Craiglockhart' AND stopb.name='London Road'
+SELECT DISTINCT R1.company, R1.num FROM route R1, route R2
+  WHERE R1.num=R2.num AND R1.company=R2.company AND R1.stop=115 AND R2.stop=137
+SELECT R1.company, R1.num FROM route R1, route R2, stops S1, stops S2
+  WHERE R1.num=R2.num AND R1.company=R2.company
+    AND R1.stop=S1.id AND R2.stop=S2.id AND S1.name='Craiglockhart' AND S2.name='Tollcross'
+SELECT DISTINCT S2.name, R2.company, R2.num FROM stops S1, stops S2, route R1, route R2
+  WHERE S1.name='Craiglockhart' AND S1.id=R1.stop AND R1.company=R2.company AND R1.num=R2.num AND R2.stop=S2.id
+
+-- https://sqlzoo.net/wiki/Window_LAG
+SELECT name, DAY(whn), confirmed, deaths, recovered FROM covid WHERE name = 'Spain' AND MONTH(whn) = 3 ORDER BY whn
+SELECT name, DAY(whn), confirmed, LAG(confirmed, 1) OVER (partition by name ORDER BY whn) AS dbf FROM covid WHERE name = 'Italy' AND MONTH(whn) = 3 ORDER BY whn
+SELECT name, DAY(whn), confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) as new
+  FROM covid WHERE name = 'Italy' AND MONTH(whn) = 3 ORDER BY whn
+SELECT name,DATE_FORMAT(whn,'%Y-%m-%d'), confirmed-LAG(confirmed,1) OVER (ORDER BY whn) "new this week" FROM covid WHERE name='Italy' and WEEKDAY(whn) = 0
+SELECT tw.name, DATE_FORMAT(tw.whn,'%Y-%m-%d'),  tw.confirmed - lw.confirmed FROM covid tw LEFT JOIN covid lw ON 
+  DATE_ADD(lw.whn, INTERVAL 1 WEEK) = tw.whn AND tw.name=lw.name WHERE tw.name = 'Italy' AND WEEKDAY(tw.whn) = 0 ORDER BY tw.whn
+SELECT name, confirmed, RANK() OVER (ORDER BY confirmed DESC) rc, deaths, RANK() OVER (ORDER BY deaths DESC) rc
+  FROM covid WHERE whn = '2020-04-20' ORDER BY confirmed DESC
+SELECT  world.name, ROUND(100000*confirmed/population,0), RANK() OVER (ORDER BY confirmed/population) AS rank
+  FROM covid JOIN world ON covid.name=world.name WHERE whn = '2020-04-20' AND population > 10000000 ORDER BY population DESC
+SELECT name,DATE_FORMAT(whn,'%Y-%m-%d'),
+  newCases AS peakNewCases
+FROM (
+SELECT name,whn,newCases,
+  RANK() OVER 
+    (PARTITION BY name ORDER BY newCases DESC) rnc
+FROM
+(
+  SELECT name, whn,
+     confirmed -
+     LAG(confirmed, 1) OVER
+      (PARTITION BY name ORDER BY whn) as newCases
+   FROM covid
+) AS x
+) AS y
+WHERE rnc = 1 AND newCases>1000
+ORDER BY whn
+
